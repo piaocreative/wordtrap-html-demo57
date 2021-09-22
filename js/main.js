b525556228f0163612523a4bcf4d88e34ebd79d8
piaocreative <johnpiao171019@gmail.com>
2021-09-22 08:47:44 +0800

    demo57-0922

---
+++ js/main.js
@@ -0,0 +1,223 @@
+$( document ).ready( function() {
+    
+    if ( $( '.logos' ).length ) {
+        $( '.logos' ).slick({
+            infinite: true,
+            slidesToShow: 7,
+            slidesToScroll: 7,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+    } 
+
+    if ( $( '.segments' ).length ) {
+        $( '.segments' ).slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+
+        $(".segment-prev").click(function(e) {
+            e.preventDefault();
+            $('.segments').slick('slickPrev');
+          })
+          
+        $(".segment-next").click(function(e) {
+            e.preventDefault();
+            $('.segments').slick('slickNext');
+        })
+    }
+
+    if ( $( '.houses' ).length ) {
+        $( '.houses' ).slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+
+        $(".house-prev").click(function(e) {
+            e.preventDefault();
+            $('.houses').slick('slickPrev');
+          })
+          
+        $(".house-next").click(function(e) {
+            e.preventDefault();
+            $('.houses').slick('slickNext');
+        })
+    }
+
+    if ( $( '.testimonials' ).length ) {
+        $( '.testimonials' ).slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+
+        $(".testimonials-prev").click(function(e) {
+            e.preventDefault();
+            $('.testimonials').slick('slickPrev');
+          })
+          
+        $(".testimonials-next").click(function(e) {
+            e.preventDefault();
+            $('.testimonials').slick('slickNext');
+        })
+    }
+
+    if ( $( '.posts' ).length ){
+        $( '.posts' ).slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+
+        $(".posts-prev").click(function(e) {
+            e.preventDefault();
+            $('.posts').slick('slickPrev');
+          })
+          
+        $(".posts-next").click(function(e) {
+            e.preventDefault();
+            $('.posts').slick('slickNext');
+        })
+    }
+
+} );
